2156c363a95f74b9162f191f78cde1102a2c6b65
Pablo Machado <pmmachadov@gmail.com>
2022-11-23 15:28:40 +0100

    En proceso.

--- ejercicios.php
+++ ejercicios.php
@@ -1,69 +1,90 @@
 <!-- ejercicios.php Muestra la tabla y la form. Si contiene HTML. -->
 
-<!-- Crea una form con nombre, apellidos, un drop down menu con paises y checkboxes con preferencias: Teatro, Libros y Cine. Un boton Enviar.
+<!-- Crea una form con nombre, apellidos, un drop down menu con paises y checkboxes con preferencias: Teatro, libros y Cine. Un boton Enviar.
 Al pulsar el botón “Enviar” el formulario deberá enviar los datos a la página
 “datos.php” empleando el método POST. Diseña a continuación dicha página para que
 muestre los valores introducidos por el usuario en cada uno de los campos, incluidos preferencias como un string.
 Estos resultados deben ser mostrados en una tabla.
 Usar: <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-<span class="error"> <?php echo $genderErr; ?></span>
+<span class="error"> <?php echo $librosErr; ?></span>
 validar el formulario completamente.
 
 -->
+
+<!-- Color rojo a los errores style -->
+
+<style>
+    .error {
+        color: #FF0000;
+    }
+</style>
 <?php
 require_once("./funlib.php");
+// convert to string
+$preferencias = implode(', ', $preferencias);
 ?>
-<form id="formulario" action="ejercicios.php" method="POST">
-    <table>
-        <tr>
-            <td>Nombre:</td>
-            <td><input type="text" name="nombre" value="<?php echo $nombre; ?>"></td>
-            <td><span class="error">
-                    <?php echo $nombreErr; ?>
-                </span></td>
-        </tr>
-        <tr>
-            <td>Apellidos:</td>
-            <td><input type="text" name="apellidos" value="<?php echo $apellidos; ?>"></td>
-            <td><span class="error">
-                    <?php echo $apellidosErr; ?>
-                </span></td>
-        </tr>
-        <tr>
-            <td>Pais:</td>
-            <td>
-                <select name="pais">
-                    <option value="España">España</option>
-                    <option value="Francia">Francia</option>
-                    <option value="Italia">Italia</option>
-                    <option value="Alemania">Alemania</option>
-                    <option value="Portugal">Portugal</option>
-                </select>
-            </td>
-            <td><span class="error">
-                    <?php echo $paisErr; ?>
-                </span></td>
-            <?php if (isset($pais)) {
-                echo "$pais";
-            } ?>
-            <td></td>
-        </tr>
-        <tr>
-            <td>Preferencias:</td>
-            <td>
-                <input type="checkbox" name="preferencias[]" value="Teatro">Teatro<br>
-                <input type="checkbox" name="preferencias[]" value="Libros">Libros<br>
-                <input type="checkbox" name="preferencias[]" value="Cine">Cine<br>
-            </td>
-            <td><span class="error">
-                    <?php echo $preferenciasErr; ?>
-                </span></td>
-            <?php if (isset($preferencias)) { echo "$preferencias"; } ?>
-        </tr>
-        <tr>
-            <td></td>
-            <td><input type="submit" name="enviar" value="Enviar"></td>
-        </tr>
 
+<table border="1" solid black>
+    <tr>
+        <td>Ejercicio</td>
+        <td>anexo2-pag39-ej1</td>
+    </tr>
+    <table BORDER border="1" solid black>
+        <br>
+        <tr>
+            <td>Nombre</td>
+            <td>
+                <?php echo $nombre; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>Apellidos</td>
+            <td>
+                <?php echo $apellidos; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>Pais</td>
+            <td>
+                <?php echo $pais; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>Preferencias</td>
+            <td>
+                <?php echo $preferencias; ?>
+            </td>
+        </tr>
     </table>
-</form>
+    <br>
+    <p><span class="error">* required field</span></p>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); "funlib.php"?>">
+        Nombre: <input type="text" Cine="nombre">
+        <span class="error">*
+            <?php echo $nombreErr; ?>
+        </span>
+        <br><br>
+        Apellidos: <input type="text" Cine="apellidos">
+        <span class="error">*
+            <?php echo $apellidosErr; ?>
+        </span>
+        <br><br>
+        Pais: <select nombre="pais">
+            <option value="España">España</option>
+            <option value="Francia">Francia</option>
+            <option value="Italia">Italia</option>
+            <option value="Alemania">Alemania</option>
+            <option value="Portugal">Portugal</option>
+            <option value="Reino Unido">Reino Unido</option>
+            <option value="Otro">Otro</option>
+        </select>
+        <span class="error">*
+            <?php echo $paisErr; ?>
+        </span>
+        <br><br>
+        <?php
+        $preferencias = array("Teatro", "Libros", "Cine");
+        foreach ($preferencias as $preferencia) {
+            echo "<input type='checkbox' name='preferencias[]' value='$preferencia'>$preferencia";
+        }
+        
--- funlib.php
+++ funlib.php
@@ -1,77 +1,58 @@
 <?php
-declare(strict_types=1); // strict requirement
-//funlib.php es el fichero para las funciones. No contiene HTML
+require_once("./ejercicios.php");
 
-// Funcion que define variables and set to empty values
-$nombre = $apellidos = $pais = $preferencias = "";
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nombre = test_input($_POST["nombre"]); // Fatal error: Uncaught Error: Call to undefined function test_input() Solution: require_once("./ejercicios.php");
-        $apellidos = test_input($_POST["apellidos"]);
-        $pais = test_input($_POST["pais"]);
-        $preferencias = test_input($_POST["preferencias"]);
-        function test_input() // Funcion que limpia los datos de entrada
-        
-        $nombre = test_input($_POST["nombre"]);
-        if (!preg_match("/^[a-zA-Z-' ]*$/",$nombre)) {
-          $nombreErr = "Only letters and white space allowed";
-        }
-        $apellidos = test_input($_POST["apellidos"]);
-        if (!preg_match("/^[a-zA-Z-' ]*$/",$apellidos)) {
-          $apellidosErr = "Only letters and white space allowed";
-        }
-        if empty ($_POST["pais"]) {
-            $paisErr = "Pais is required";
-        } else {
-            $pais = test_input($_POST["pais"]);
-        }
-        if (empty($_POST["preferencias"])) {
-            $preferenciasErr = "Preferencias is required";
-        } else {
-            $preferencias = test_input($_POST["preferencias"]);
-        }
+?>
 
-// Notice that at the start of the script, we check whether the form has been submitted using $_SERVER["REQUEST_METHOD"]. If the REQUEST_METHOD is POST, then the form has been submitted - and it should be validated. If it has not been submitted, skip the validation and display a blank form. However, in the example above, all input fields are optional. The script works fine even if the user does not enter any data.
+<?php
+$nombreErr = "Por favor escribe tu nombre. Solo se permiten letras y espacios en blanco";
+$apellidosErr = "Por favor escribe tus apellidos. Solo se permiten letras y espacios en blanco";
+$paisErr = "Por favor, seleccione un pais";
+$preferenciasErr = "Por favor seleciona una preferencia";
+$nombre = $apellidos = $pais = $preferencias = $pelicula = $cine = $libros = "";
 
-// define variables and set to empty values
-$nombreErr = $apellidosErr = $paisErr = $preferenciasErr = "";
-$nombre = $apellidos = $pais = $preferencias = "";
+function test_input($data)
+{
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+$preferencias = array();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["nombre"])) {
-    $nombreErr = "nombre is required";
+    echo $nombreErr;
   } else {
     $nombre = test_input($_POST["nombre"]);
-    // check if nombre only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z-' ]*$/",$nombre)) {
-      $nombreErr = "Only letters and white space allowed";
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/", $nombre)) {
+      echo $nombreErr;
     }
   }
 
-    if (empty($_POST["apellidos"])) {
-        $apellidosErr = "apellidos is required";
-    } else {
-        $apellidos = test_input($_POST["apellidos"]);
-        // check if apellidos only contains letters and whitespace
-        if (!preg_match("/^[a-zA-Z-' ]*$/", $apellidos)) {
-        $apellidosErr = "Only letters and white space allowed";
-        }
+  if (empty($_POST["apellidos"])) {
+    echo $apellidosErr;
+  } else {
+    $apellidos = test_input($_POST["apellidos"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/", $apellidos)) {
+      echo $apellidosErr;
     }
-
-    if (empty($_POST["pais"])) {
-        $paisErr = "pais is required";
+  }
+  if (isset($_POST['submit'])) {
+    if (!empty($_POST['pais'])) {
+      foreach ($_POST['pais'] as $selected) {
+        echo '  ' . $selected;
+      }
     } else {
-        $pais = test_input($_POST["pais"]);
-    
+      echo $preferenciasErr;
     }
-}
+  }
 
-if (empty ($_POST["preferencias"])) {
-    $preferenciasErr = "preferencias is required";
-} else {
-    $preferencias = test_input($_POST["preferencias"]);
-}
 
-echo $nombre;
-echo $apellidos;
-echo $pais;
-echo $preferencias;
+
+
+
+
+}
